d05bc11c9bfaea6456e62ae2153f769300709e38
Teo <teo.xu@gzmediaclick.com>
2019-02-13 17:12:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # breathejs
-一个类似呼吸节奏的动画库
+一个类似呼吸节奏的动画库，支持css3 transform 多种属性
+
+## 使用
+
+```js
+// js
+import Breathe from 'breathe'
+const breathe = new Breathe()
+breathe.init()
+```
+
+```html
+<!-- html -->
+<div data-breathe="scale(1.2, 1.2)">缩放</div>
+<div data-breathe="rotate(30deg)">旋转</div>
+<div data-breathe="translate(10px, 10px)">位移</div>
+<div data-breathe="skew(30deg)">倾斜</div>
+<div data-breathe="scale(1.2, 1.2);rotate(30deg)">多动画用‘;’隔开</div>
+<div data-breathe="rotate(30deg);origin(left top)">改变旋转轴</div>
+<div data-breathe="rotate(30deg);duration(2000)">更默认为1500毫秒的动画时间</div>
+<div data-breathe="rotate(30deg);timing(ease)">更默认为linear的动画曲线</div>
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+class Breathe {
+  init () {
+    const el = document.body.querySelectorAll('*')
+    el.forEach((el, i) => {
+      if (el.dataset.breathe) {
+        this.doAnimation(el)
+      }
+    })
+  }
+
+  doAnimation (el) {
+    const data = el.dataset.breathe.split(';')
+    let transform = ''
+    let duration = 1500
+    let timing = 'linear'
+    let origin = '50% 50% 0'
+    data.forEach(o => {
+      switch (true) {
+        case /scale/.test(o):
+          transform += o + ''
+          break
+        case /rotate/.test(o):
+          transform += o + ''
+          break
+        case /translate/.test(o):
+          transform += o + ''
+          break
+        case /skew/.test(o):
+          transform += o + ''
+          break
+        case /duration/.test(o):
+          duration = /[^()]+(?=\))/.exec(o)[0]
+          break
+        case /timing/.test(o):
+          timing = /[^()]+(?=\))/.exec(o)[0]
+          break
+        case /origin/.test(o):
+          origin = /[^()]+(?=\))/.exec(o)[0]
+          break
+      }
+    })
+    const transition = `transform ${timing} ${duration / 1000}s`
+    el.style['transition'] = transition
+    el.style['-ms-transition'] = transition
+    el.style['-webkit-transition'] = transition
+    el.style['-o-transition'] = transition
+    el.style['-moz-transition'] = transition
+
+    el.style['transform-origin'] = origin
+    el.style['-ms-transform-origin'] = origin
+    el.style['-webkit-transform-origin'] = origin
+    el.style['-o-transform-origin'] = origin
+    el.style['-moz-transform-origin'] = origin
+    setInterval(() => {
+      if (el.style.transform) {
+        el.style['transform'] = ''
+        el.style['-ms-transform'] = ''
+        el.style['-webkit-transform'] = ''
+        el.style['-o-transform'] = ''
+        el.style['-moz-transform'] = ''
+      } else {
+        el.style['transform'] = transform
+        el.style['-ms-transform'] = transform
+        el.style['-webkit-transform'] = transform
+        el.style['-o-transform'] = transform
+        el.style['-moz-transform'] = transform
+      }
+    }, duration)
+  }
+}
+export default Breathe

6eb4c35df4bd0c04fb18bc46731de9386e7de952
Teo <teo.xu@gzmediaclick.com>
2019-02-14 11:16:35 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+export default {
+  input: './src/main.js',
+  output: {
+    file: './dist/breathe.js',
+    format: 'umd',
+    name: 'Breathe'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })/* ,
+    uglify() */
+  ]
+}
--- src/main.js
+++ src/main.js
@@ -68,3 +68,4 @@ class Breathe {
     }, duration)
   }
 }
+export default Breathe

fe4b64cc79c6859c96a9c90c6d8ad4bb6bdac751
Teo <teo.xu@gzmediaclick.com>
2019-02-21 18:15:45 +0800

    init

--- clipic.js
+++ clipic.js
@@ -33,7 +33,15 @@
       classCallCheck(this, Clipic);
 
       this.options = options;
+      this.scaleX = 1;
+      this.scaleY = 1;
+      this.rotate = 0;
       this.init();
+      this.clipicFrame = this.getId('clipicFrame');
+      this.clipic = this.getId('clipic');
+      this.clipicCancel = this.getId('clipicCancel');
+      this.clipicImg = document.createElement('img');
+      this.clipicFrame.appendChild(this.clipicImg);
     }
 
     createClass(Clipic, [{
@@ -43,11 +51,16 @@
         this.createHtml();
       }
     }, {
+      key: 'getId',
+      value: function getId(id) {
+        return document.getElementById(id);
+      }
+    }, {
       key: 'createStyle',
       value: function createStyle() {
         var style = document.createElement('style');
         style.type = 'text/css';
-        var css = '\n      .clipic-body {\n        background: #1c1c1c;\n        position: fixed;\n        width: 100%;\n        height: 100%;\n        top: 0;\n        left: 0;\n        transform: translate(0, 100%);\n        transition: transform 0.4s;\n        -webkit-touch-callout: none;\n        -webkit-user-select: none;\n        box-sizing: border-box;\n      }\n      .clipic-body * {\n        box-sizing: border-box;\n      }\n      .clipic-operation-bar {\n        display: flex;\n        color: #f2f2f2;\n        justify-content: space-between;\n        position: absolute;\n        width: 100%;\n        bottom: 0;\n        left: 0;\n      }\n      .clipic-operation-bar [role="button"] {\n        padding: 15px 20px;\n        font-size: 1.2em;\n      }\n      .clipic-frame {\n        height: 300px;\n        margin: 30px;\n        background: #f2f2f2;\n      }\n      .clipic-frame img {\n        width: 100%;\n      }\n      .clipic-cancel {\n        color: #3680fd;\n      }\n      .clipic-confirm{\n        color: #23c667;\n      }\n    '.trim();
+        var css = '\n      .clipic-body {\n        background: #1c1c1c;\n        position: fixed;\n        width: 100%;\n        height: 100%;\n        top: 0;\n        left: 0;\n        transform: translate(0, 100%);\n        transition: 0.4s;\n        -webkit-touch-callout: none;\n        -webkit-user-select: none;\n        box-sizing: border-box;\n      }\n      .clipic-body * {\n        box-sizing: border-box;\n      }\n      .clipic-operation-bar {\n        display: flex;\n        color: #f2f2f2;\n        justify-content: space-between;\n        position: absolute;\n        width: 100%;\n        bottom: 0;\n        left: 0;\n      }\n      .clipic-operation-bar [role="button"] {\n        padding: 15px 20px;\n        font-size: 1em;\n      }\n      .clipic-frame {\n        height: 300px;\n        margin: 30px;\n        background: #f2f2f2;\n        overflow: hidden;\n      }\n      .clipic-frame img {\n        transition: 1s;\n      }\n      .clipic-cancel {\n        color: #3680fd;\n      }\n      .clipic-confirm{\n        color: #23c667;\n      }\n    '.trim();
         style.innerHTML = css;
         document.getElementsByTagName('HEAD').item(0).appendChild(style);
       }
@@ -57,28 +70,53 @@
         var div = document.createElement('div');
         div.className = 'clipic-body';
         div.setAttribute('id', 'clipic');
-        var html = '\n      <div class="clipic-frame" id="clipicFrame"></div>\n      <div class="clipic-operation-bar">\n        <div class="clipic-cancel" role="button">\u53D6\u6D88</div>\n        <div class="clipic-confirm" role="button">\u5B8C\u6210</div>\n      </div>\n    '.trim();
+        var html = '\n      <div class="clipic-frame" id="clipicFrame"></div>\n      <div class="clipic-operation-bar">\n        <div class="clipic-cancel" id="clipicCancel" role="button">\u53D6\u6D88</div>\n        <div class="clipic-confirm" id="clipicConfirm" role="button">\u5B8C\u6210</div>\n      </div>\n    '.trim();
         div.innerHTML = html;
         document.body.appendChild(div);
       }
     }, {
       key: 'getImage',
       value: function getImage() {
+        var _this = this;
+
         var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
         var newOptions = options;
         if (this.options) {
           newOptions = Object.assign(this.options, options);
         }
-        var clipicFrame = document.getElementById('clipicFrame');
-        var clipicFrameW = newOptions.width || clipicFrame.clientWidth;
-        var clipicFrameH = newOptions.height || clipicFrame.clientHeight;
-        clipicFrame.style.height = clipicFrame.clientWidth / (clipicFrameW / clipicFrameH) + 'px';
-        var img = document.createElement('img');
-        img.src = newOptions.src;
-        clipicFrame.appendChild(img);
-        var clipic = document.getElementById('clipic');
-        clipic.style['transform'] = 'translate(0, 0)';
+        var clipicFrameW = newOptions.width || this.clipicFrame.clientWidth;
+        var clipicFrameH = newOptions.height || this.clipicFrame.clientHeight;
+        this.clipicFrame.style.height = this.clipicFrame.clientWidth / (clipicFrameW / clipicFrameH) + 'px';
+        this.clipicImg.src = newOptions.src;
+        this.clipic.style['transform'] = 'translate(0, 0)';
+        setTimeout(function () {
+          _this.setMiniSize();
+        }, 400);
+        this.clipicCancel.addEventListener('click', function () {
+          _this.cancel();
+        });
+      }
+    }, {
+      key: 'setMiniSize',
+      value: function setMiniSize() {
+        var w = this.clipicFrame.clientWidth;
+        var h = this.clipicFrame.clientHeight;
+        var imgW = this.clipicImg.clientWidth;
+        var imgH = this.clipicImg.clientHeight;
+        if (imgH < h) {
+          this.clipicImg.style.width = h * (imgW / imgH) + 'px';
+          this.clipicImg.style.height = h + 'px';
+        }
+        if (imgW < w) {
+          this.clipicImg.style.width = w + 'px';
+          this.clipicImg.style.height = w * (imgW / imgH) + 'px';
+        }
+      }
+    }, {
+      key: 'cancel',
+      value: function cancel() {
+        this.clipic.style['transform'] = 'translate(0, 100%)';
       }
     }]);
     return Clipic;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 class Clipic {
   constructor(options) {
     this.options = options
+    this.scaleX = 1
+    this.scaleY = 1
+    this.rotate = 0
     this.init()
+    this.clipicFrame = this.getId('clipicFrame')
+    this.clipic = this.getId('clipic')
+    this.clipicCancel = this.getId('clipicCancel')
+    this.clipicImg = document.createElement('img')
+    this.clipicFrame.appendChild(this.clipicImg)
   }
 
   init() {
     this.createStyle()
     this.createHtml()
+  }
+
+  getId(id) {
+    return document.getElementById(id)
   }
 
   createStyle() {
@@ -21,7 +33,7 @@ class Clipic {
         top: 0;
         left: 0;
         transform: translate(0, 100%);
-        transition: transform 0.4s;
+        transition: 0.4s;
         -webkit-touch-callout: none;
         -webkit-user-select: none;
         box-sizing: border-box;
@@ -46,9 +58,10 @@ class Clipic {
         height: 300px;
         margin: 30px;
         background: #f2f2f2;
+        overflow: hidden;
       }
       .clipic-frame img {
-        width: 100%;
+        transition: 1s;
       }
       .clipic-cancel {
         color: #3680fd;
@@ -71,8 +84,8 @@ class Clipic {
     var html = `
       <div class="clipic-frame" id="clipicFrame"></div>
       <div class="clipic-operation-bar">
-        <div class="clipic-cancel" role="button">取消</div>
-        <div class="clipic-confirm" role="button">完成</div>
+        <div class="clipic-cancel" id="clipicCancel" role="button">取消</div>
+        <div class="clipic-confirm" id="clipicConfirm" role="button">完成</div>
       </div>
     `.trim()
     div.innerHTML = html
@@ -84,15 +97,36 @@ class Clipic {
     if (this.options) {
       newOptions = Object.assign(this.options, options)
     }
-    var clipicFrame = document.getElementById('clipicFrame')
-    var clipicFrameW = newOptions.width || clipicFrame.clientWidth
-    var clipicFrameH = newOptions.height || clipicFrame.clientHeight
-    clipicFrame.style.height = clipicFrame.clientWidth / (clipicFrameW / clipicFrameH) + 'px'
-    var img = document.createElement('img')
-    img.src = newOptions.src
-    clipicFrame.appendChild(img)
-    var clipic = document.getElementById('clipic')
-    clipic.style['transform'] = 'translate(0, 0)'
+    var clipicFrameW = newOptions.width || this.clipicFrame.clientWidth
+    var clipicFrameH = newOptions.height || this.clipicFrame.clientHeight
+    this.clipicFrame.style.height = this.clipicFrame.clientWidth / (clipicFrameW / clipicFrameH) + 'px'
+    this.clipicImg.src = newOptions.src
+    this.clipic.style['transform'] = 'translate(0, 0)'
+    setTimeout(() => {
+      this.setMiniSize()
+    }, 400)
+    this.clipicCancel.addEventListener('click', () => {
+      this.cancel()
+    })
+  }
+
+  setMiniSize() {
+    var w = this.clipicFrame.clientWidth
+    var h = this.clipicFrame.clientHeight
+    var imgW = this.clipicImg.clientWidth
+    var imgH = this.clipicImg.clientHeight
+    if (imgH < h) {
+      this.clipicImg.style.width = h * (imgW / imgH) + 'px'
+      this.clipicImg.style.height = h + 'px'
+    }
+    if (imgW < w) {
+      this.clipicImg.style.width = w + 'px'
+      this.clipicImg.style.height = w * (imgW / imgH) + 'px'
+    }
+  }
+
+  cancel() {
+    this.clipic.style['transform'] = 'translate(0, 100%)'
   }
 }
 export default Clipic

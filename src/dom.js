391a132514f2819c2d813b12b4902987c1ce5170
Teo <teo.xu@gzmediaclick.com>
2019-02-25 17:19:45 +0800

    init

---
+++ src/dom.js
@@ -0,0 +1,8 @@
+const dom = `
+    <div class="clipic-frame" id="clipicFrame"></div>
+    <div class="clipic-operation-bar">
+      <div class="clipic-cancel" id="clipicCancel" role="button">取消</div>
+      <div class="clipic-confirm" id="clipicConfirm" role="button">完成</div>
+    </div>
+  `
+export default dom
